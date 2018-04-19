97be0a832b5cd6db3f01b05cba295dd66e58860c
Pablo Rocha Moreira <prm.gredes@gmail.com>
2018-04-19 04:44:41 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -3,7 +3,7 @@ def my_select(collection)
  i = 0
  selected_list = []
  while i < collection.size
-     selected << yield(collection[i])
+     selected = yield(collection[i])
      selected_list << collection[i] if selected
      i+=1
  end
